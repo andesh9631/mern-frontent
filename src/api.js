15003029f311ba4e4e91300f7e6c3176c9a6109d
Andesh Kumar Sah <sahandesh477@gmail.com>
2026-01-29 18:06:06 +0530

    frontend env + socket fix

--- src/api.js
+++ src/api.js
@@ -1,8 +1,11 @@
-const API_URL =
-  process.env.REACT_APP_API_URL ||
-  "https://bidpulse-live-bidding-platform-2.onrender.com";
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const fetchItems = async () => {
   const res = await fetch(`${API_URL}/items`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch items");
+  }
+
   return res.json();
 };
--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,7 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL =
-  process.env.REACT_APP_SOCKET_URL ||
-  "https://bidpulse-live-bidding-platform-2.onrender.com";
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
 
 export const socket = io(SOCKET_URL, {
-  transports: ["websocket"],
+  transports: ["websocket"], // prevents polling errors
 });
